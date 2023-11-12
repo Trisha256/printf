517bd73a6a3fad0fcc35a2e6b34bb0dd737cb9de
Sapphire-Creativity <sapphirecreativity@gmail.com>
2023-11-11 23:21:42 -0800

    printf

--- printf.c
+++ printf.c
@@ -3,73 +3,54 @@
 #include <unistd.h>
 
 /**
- * _printf - This function is a custom printf function
- * @format: This is the format string
- * @...: This represents variable number of arguments
- * Return: To return numbers of charactrs printed
+ * _printf - The custom printf function
+ * @format: The format string
+ * @...: The variable number of arguments
+ * Return: Number of characters printed
  */
 
 int _printf(const char *format, ...)
-i
-        int character_print = 0;
-        va_list list_args_for_char;
-        va_start(list_args_for_char, format);
+{
+	int character_print = 0;
+	va_list list_args_for_char;
 
-        if (format == NULL)
-        {
-                va_end(list_args_for_char);
-                return (-1);
-        }
+	if (format == NULL)
+		{
+			return (-1);
+		}
+	va_start(list_args_for_char, format);
 
-        while(*format)
-        {
-                if (*format != '%' || (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i')))
-                {
-                        write(1, format, 1);
-                        character_print++;
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			character_print++;
+		}
+		
+		else
+		{
+			format++;
+		}
 
-                        if (*format == 'd' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
-                        {
-                                int num = va_arg(list_args_for_char, int);
+		if (*format == 'd')
+		{
+			int num = va_arg(list_args_for_char, int);
+			character_print += num;
+		}
 
-                                int temp = num;
-                                int count = (temp == 0) ? 1 : 0;
+		else if (*format == '\0')
+		{
+			break;
+		}
+		
+		else
+		{
+			character_print += (*format, va_arg(list_args_for_char, num));
+		}
+	}
 
-                                while (temp != 0)
-                                {
-                                        temp /= 10;
-                                        count++;
-                                }
+	va_end(list_args_for_char);
 
-                                char digits[count];
-
-                                for (int 1 = count - 1; i >= 0; i--)
-                                {
-                                        digits[1] = (num % 10) + '0';
-                                        num /= 10;
-                                }
-
-                                for (int 1 = 0; i < count; i++)
-                                {
-                                        write(1, &digits[i], 1);
-                                        character_print++;
-                                }
-
-                                format++;
-
-                        }
-                }
-
-                else if (*format == '\0')
-                {
-                        break;
-                }
-
-                format++;
-        }
-
-        va_end(list_args_for_char);
-
-        return character_print;
-
+	return character_print;
 }
