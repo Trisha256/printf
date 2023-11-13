fd6d21098cf7225d060f084fa9eb9423298daff1
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2023-11-13 07:49:36 -0800

    Task 0

--- print_int.c
+++ print_int.c
@@ -8,40 +8,8 @@
 
 int _print_int(va_list args)
 {
-int  m = va_arg(args, int);
-int numb, end = m % 10, value, _exp = 1;
-int i = 1;
-
-m = m / 10;
-numb = m;
-
-if (end < 0)
-{
-_putchar('-');
-numb = -numb;
-m = -m;
-end = -end;
-i++;
-}
-if (numb > 0)
-{
-while (numb / 10 != 0)
-{
-_exp = _exp * 10;
-numb = numb / 10;
-
-}
-numb = m;
-while (_exp > 0)
-{
-value = numb / _exp;
-_putchar(value + '0');
-_exp = _exp / 10;
-i++;
-}
-}
-_putchar(end + '0');
-return (i);
+int num = va_arg(args, int);
+return (printf("%d", num));
 }
 
 #include "main.h"
@@ -53,40 +21,8 @@ return (i);
 
 int _print_dec(va_list args)
 {
-int  m = va_arg(args, int);
-int numb, end = m % 10, value, _exp = 1;
-int i = 1;
-
-m = m / 10;
-numb = m;
-
-if (end < 0)
-{
-_putchar('-');
-numb = -numb;
-m = -m;
-end = -end;
-i++;
-}
-if (numb > 0)
-{
-while (numb / 10 != 0)
-{
-_exp = _exp * 10;
-numb = numb / 10;
-
-}
-numb = m;
-while (_exp > 0)
-{
-value = numb / _exp;
-_putchar(value + '0');
-_exp = _exp / 10;
-i++;
-}
-}
-_putchar(end + '0');
-return (i);
+int num = va_arg(args, int);
+return (printf("%i", num));
 }
 
 
