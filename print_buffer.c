81a331641d6e2f410e7fa45977369cca1b79288e
Sapphire-Creativity <sapphirecreativity@gmail.com>
2023-11-15 05:10:51 -0800

    print_buffer.c

--- print_buffer.c
+++ print_buffer.c
@@ -10,11 +10,11 @@
 int print_buffer(char *buf, unsigned int nbuf)
 {
 	static char internal_buffer[10240];
-	static unsigned int buf_pos = 0;
-
+	static unsigned int buf_pos;
 	unsigned int remaining_space = 1024 - buf_pos;
+
 	if (nbuf > 1024)
-		return -1;
+		return (-1);
 
 	if (nbuf > remaining_space)
 	{
@@ -25,18 +25,5 @@ int print_buffer(char *buf, unsigned int nbuf)
 	memcpy(internal_buffer + buf_pos, buf, nbuf);
 	buf_pos += nbuf;
 
-	return 0;
-}
-/**
-* main - Entry point
-* Return: Always p (Success)
-*/
-int main()
-{
-	char message[] = "This is a test message.";
-	print_buffer(message, strlen(message));
-
-	print_buffer("", 0);
-
-	return 0;
+	return (0);
 }
